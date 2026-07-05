13a1bb308e71b8bfeaefd43e90a9e1450b9e739c
JoeRagheb29 <youssifragheb838@gmail.com>
2026-07-05 05:36:21 +0300

    create Dashboard component

---
+++ src/features/Organization/pages/DashboardPage.jsx
@@ -0,0 +1,11 @@
+
+function DashboardPage() {
+  return (
+  <div className="p-2">
+    <h1 className="text-xl font-semibold text-slate-800 mb-1">Dashboard</h1>
+    <p className="text-slate-400 text-sm">Module under construction.</p>
+  </div>
+  )
+}
+
+export default DashboardPage;
